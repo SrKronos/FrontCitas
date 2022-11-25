1ff5111b6c66deaf0382970b5a56f9ef4f39d024
Leonardo Loor <LeonardoLoor1988@gmail.com>
2022-11-25 13:02:52 -0500

    ng

--- src/app/components/card-doctor/card-doctor.component.ts
+++ src/app/components/card-doctor/card-doctor.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,ViewChild,ViewChildren,ElementRef,QueryList,HostListener } from '@angular/core';
+import { Component, OnInit,ViewChild,ViewChildren,ElementRef,QueryList,HostListener, EventEmitter } from '@angular/core';
 import { ServicioCitasService } from 'src/app/services/servicio-citas.service';
 import * as moment from 'moment';
 @Component({
@@ -10,6 +10,7 @@ export class CardDoctorComponent implements OnInit {
   listaDoctores: any = [];
   numeroitem: string = "";
   animar: string= "animar";
+
   constructor(private servicio: ServicioCitasService) {
 
   }
@@ -18,8 +19,9 @@ export class CardDoctorComponent implements OnInit {
     moment.locale("es");
     const hoy = moment();//.format('YYYY-MM-DD HH:MM:SS');
     const tardesito = hoy.clone().add(30,'minutes');
-    const formato = 'YYYY-MM-DD hh:mm:ss';
-    const fechaInicio = hoy.format(formato);
+    const antesito = hoy.clone().subtract(30,'minutes');
+    const formato = 'YYYY-MM-DD HH:mm:ss';
+    const fechaInicio = antesito.format(formato);
     const fechaFinal = tardesito.format(formato);
     this.servicio.getJson("http://localhost/wsCitasMedicas/citasg.php?fechaInicio=" + fechaInicio + "&fechaFinal=" + fechaFinal).subscribe((res: any) => {
       this.listaDoctores = res;
@@ -27,10 +29,31 @@ export class CardDoctorComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {
-    this.recargarLista();
+  public ObtenerDoctoresAsync(){
+    this.listaDoctores = this.recargarLista();
   }
 
+  recargarLista = async ()=>{
+    moment.locale("es");
+    const hoy = moment();//.format('YYYY-MM-DD HH:MM:SS');
+    const tardesito = hoy.clone().add(30,'minutes');
+    const antesito = hoy.clone().subtract(30,'minutes');
+    const formato = 'YYYY-MM-DD HH:mm:ss';
+    const fechaInicio = antesito.format(formato);
+    const fechaFinal = tardesito.format(formato);
+    let respuesta = await (await fetch("http://localhost/wsCitasMedicas/citasg.php?fechaInicio="+fechaInicio+"&fechaFinal="+fechaFinal)).json();
+    this.listaDoctores = respuesta;
+  }
+  
+  ngOnInit(): void {
+    this.ObtenerDoctoresAsync();
+    setInterval(()=>{
+      this.listaDoctores=[];
+      this.recargarLista();
+    },200000);
+  }
+
+  /*
   recargarLista(): void {
     this.obtenerDoctores();
     setInterval(() => {
@@ -39,6 +62,6 @@ export class CardDoctorComponent implements OnInit {
       this.obtenerDoctores()
     }, 1800000 );//cada 10min 900000=15min  1800000=30min
   }
-
+*/
 }
 
--- src/app/components/card-paciente/card-paciente.component.ts
+++ src/app/components/card-paciente/card-paciente.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { ServicioCitasService } from 'src/app/services/servicio-citas.service';
 import { DatePipe } from '@angular/common';
 import * as moment from 'moment';
@@ -9,35 +9,61 @@ import * as moment from 'moment';
 })
 export class CardPacienteComponent implements OnInit {
   @Input() nombredoctor: string = "";
+  @Output() numeroPacientes = new EventEmitter<number>();
   listaPacientes: any = [];
   constructor(private servicio: ServicioCitasService, private datepipe: DatePipe) { }
-
+  
   public obtenerPacientes() {
     moment.locale("es");
     const hoy = moment();//.format('YYYY-MM-DD HH:MM:SS');
     const tardesito = hoy.clone().add(30,'minutes');
+    const antesito = hoy.clone().subtract(30,'minutes');
     const formato = 'YYYY-MM-DD hh:mm:ss';
-    const fechaInicio = hoy.format(formato);
+    const fechaInicio = antesito.format(formato);
     const fechaFinal = tardesito.format(formato);
     let urlws = "http://localhost/wsCitasMedicas/citasg.php?doctor="+this.nombredoctor+"&fechaInicio="+fechaInicio+"&fechaFinal="+fechaFinal;
     this.servicio.getJson(urlws).subscribe((res: any) => {
-      this.listaPacientes = res;
+    this.listaPacientes = res;
+    this.enviarNPacientes();
   });
   }
 
-  ngOnInit(): void {
-    //this.obtenerPacientes();
-    this.recargarLista();
+  public ObtenerPacientesAsync(){
+    this.listaPacientes = this.recargarLista();
   }
 
+  recargarLista = async ()=>{
+    moment.locale("es");
+    const hoy = moment();//.format('YYYY-MM-DD HH:MM:SS');
+    const tardesito = hoy.clone().add(30,'minutes');
+    const antesito = hoy.clone().subtract(30,'minutes');
+    const formato = 'YYYY-MM-DD HH:mm:ss';
+    const fechaInicio = antesito.format(formato);
+    const fechaFinal = tardesito.format(formato);
+    let respuesta = await (await fetch("http://localhost/wsCitasMedicas/citasg.php?doctor="+this.nombredoctor+"&fechaInicio="+fechaInicio+"&fechaFinal="+fechaFinal)).json();
+    this.listaPacientes = respuesta;
+} 
+
+  ngOnInit(): void {
+    this.ObtenerPacientesAsync();
+    setInterval(()=>{
+      this.listaPacientes=[];
+      this.recargarLista();
+    },100000);
+  }
+
+  enviarNPacientes(){
+    this.numeroPacientes.emit(this.listaPacientes.length);
+  }
+
+  /*
   recargarLista(): void {
-    this.obtenerPacientes();    
     setInterval(() => {
       console.log("Cargando Pacientes...");
       this.listaPacientes = [];
       this.obtenerPacientes()
-    }, 900000); //900000=15min
+    }, 1500); //900000=15min
   }
-
-
+  
+  */
 }
